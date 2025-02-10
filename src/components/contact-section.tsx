7317510569932992e695599580ebafa7c769636a
ParraFernand <humanoid199o.fp@gmail.com>
2025-02-10 15:57:04 -0400

    solveerrors1

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -24,7 +24,7 @@ export function ContactSection() {
               <h2 className="text-4xl md:text-5xl font-bold text-white">
                 Opportunities
                 <br />
-                don't happen
+                dont happen
               </h2>
               <div className="space-y-2">
                 <p className="text-3xl md:text-4xl font-bold text-brand-orange">YOU</p>
