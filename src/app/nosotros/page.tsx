971c1f39d03877c43f51eb2420f43968315c174b
ParraFernand <humanoid199o.fp@gmail.com>
2025-03-04 23:25:13 -0400

    Eliminando importaciones no utilizadas y mejorando uso de imágenes

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,7 +1,5 @@
 import { Hero } from '@/components/hero_ntrs'
 import { SobreNosotros } from '@/components/sobre-nosotros'
-import { GroupBanner } from "@/components/group-banner"
-import { Ubicacion } from '@/components/ubicacion'
 import React from 'react'
 
 const page = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { Hero } from "@/components/hero"
 import { Carousel } from "@/components/carousel"
-import { QuoteCards } from "@/components/quote-cards"
-import { TransportModes } from "@/components/transport-modes"
 import { ValuesSection } from "@/components/values-section"
 
-import { NewsGrid } from "@/components/news-grid"
-import { ContactSection } from "@/components/contact-section"
+
 
 export default function Home() {
   return (
